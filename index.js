6f422ed36c0572401a3ce7df49a429621463c4ca
tsomielvis <elvis.muthomi@studentmoringaschool.com>
2024-07-11 10:23:10 +0300

    latest

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+  cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+  cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+function appendCat(name) {
+  const newArray = cats.slice();
+  newArray.push(name);
+  return newArray;
+}
+function prependCat(name) {
+  const newArray = cats.slice();
+  newArray.unshift(name);
+  return newArray;
+}
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+function removeFirstCat() {
+  return cats.slice(1);
+}
